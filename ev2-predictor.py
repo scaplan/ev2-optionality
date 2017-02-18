d8272a6bf3d7d7b291b4fa759140a2e1d6c6eb7f
scaplan <github@spencercaplan.org>
2017-02-18 17:56:44 -0500

    Iterate through .xml and print words grouped by sentence

--- ev2-predictor.py
+++ ev2-predictor.py
@@ -7,13 +7,45 @@ sys.setdefaultencoding('utf-8')
 import unicodedata
 from unicodedata import normalize
 
+def evalSentence(sentence):
+	for currWord, currPOS in sentence:
+		print currWord + ' ' + str(currPOS)
+
 def interateCorpus(fileName):
+	totalTokens = 0
+	typeDict = {}
 	with open(fileName, 'r') as currFile:
+		currSentence = []
 		for currLine in currFile:
 			if not currLine:
 				continue
 			currLineTokens = currLine.split()
-			print currLine
+			if currLineTokens[0] == "</sentence>":
+				# we've finished the previous sentence so we should 
+				# pass the sentence to the evaluation function
+				# and then clear the "currSentence" list
+				evalSentence(currSentence)
+				currSentence = []
+
+			# check if we're reading in a word
+			if currLineTokens[0] == "<w":
+				currPosRaw = currLineTokens[1]
+				currWordRaw = currLineTokens[-1]
+
+				currWordClean = currWordRaw[currWordRaw.find(">")+1:currWordRaw.find("<")]
+				currPosClean = currPosRaw[currPosRaw.find("\"")+1:-1]
+				currPair = (currWordClean, currPosClean)
+
+				if (currWordRaw in typeDict):
+					typeDict[currWordRaw] = typeDict[currWordRaw] + 1
+				else:
+					typeDict[currWordRaw] = 1
+				totalTokens += 1
+				currSentence.append(currPair)
+			# else continue
+			# print currLine
+	print (str(totalTokens) + ' total tokens')
+	print (str(len(typeDict)) + ' total types')
 
 ##
 ## Main method block
